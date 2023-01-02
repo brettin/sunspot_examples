87d7af1ead6ddd23898bdd9dfff5b9d996031b27
Thomas Brettin <brettin@anl.gov>
2023-01-02 20:25:52 +0000

    pep8

--- sunspot_train.py
+++ sunspot_train.py
@@ -1,28 +1,31 @@
+from tensorflow import keras
+import tensorflow as tf
+import os
 import argparse
 psr = argparse.ArgumentParser(description="input dataframe file")
-psr.add_argument("--infile", default="infile", help="a file of filenames indexed with world rank")
+psr.add_argument("--infile", default="infile",
+                 help="a file of filenames indexed with world rank")
 args = vars(psr.parse_args())
 print(args)
 
 
-import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-print('{}: ZE_AFFINITY_MASK: {}'.format(os.getenv('PMIX_RANK'), os.environ['ZE_AFFINITY_MASK']))
+print('{}: ZE_AFFINITY_MASK: {}'.format(
+    os.getenv('PMIX_RANK'), os.environ['ZE_AFFINITY_MASK']))
 
 
-import tensorflow as tf
-from tensorflow import keras
 print('{}: using tensorflow {}'.format(os.getenv('PMIX_RANK'), tf.__version__))
 
 
 with open(args['infile']) as infile:
     lines = [line.rstrip() for line in infile]
-print("{}: infile={}".format(os.getenv('PMIX_RANK'),lines[ int(os.getenv('PMIX_RANK')) ] ))
-
+print("{}: infile={}".format(os.getenv('PMIX_RANK'),
+      lines[int(os.getenv('PMIX_RANK'))]))
 
 
 strategy = tf.distribute.MirroredStrategy()
-print('{}: Number of devices: {}'.format(os.getenv('PMIXX_RANK'), strategy.num_replicas_in_sync))
+print('{}: Number of devices: {}'.format(
+    os.getenv('PMIXX_RANK'), strategy.num_replicas_in_sync))
 
 with strategy.scope():
     print("{}: defining and compiling model".format(os.getenv('PMIX_RANK')))
